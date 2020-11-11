e6284341b570b6ec8ccf7d4a66771d3f4372565c
장희수 <aaajhs@pubg.com>
2020-11-11 18:50:53 +0900

    Init

---
+++ wschat/wschat/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using wschat.Models;
+
+namespace wschat.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View("InsertUserName");
+        }
+
+        [HttpPost]
+        public IActionResult Index(string username)
+        {
+            return View("Index", username);
+        }
+    }
+}
